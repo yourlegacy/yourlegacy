ad4de634a574b61ea38b9134327745a20efd2c0c
Jon Amar <jon@solomongiles.com>
2013-09-30 15:44:54 +0200

    youtube popcorn integration

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	// Smooth Scrolling
 	var $root = $('html, body');
 	$('a').click(function() {
 	    var href = $.attr(this, 'href');
@@ -8,5 +10,27 @@ $(document).ready(function(){
 	        window.location.hash = href;
 	    });
 	    return false;
+	});
+
+	// Popcorn
+	var videoInterview = Popcorn.youtube(
+		'#video-container',
+		'http://www.youtube.com/watch?v=CxvgCLgwdNk' 
+	);
+
+	videoInterview.on('ended', function(){
+		$(".video-overlay").addClass('active');
+		$(".video-overlay").addClass('post');
+		$(".video-overlay").removeClass('pre');
+	});
+
+	$( ".play-me" ).click(function() {
+ 		$(".video-overlay").removeClass('active');
+ 		$(".video-overlay").toggleClass('pre');
+  		videoInterview.play();
+	});
+	
+	$( ".play-me.pre" ).click(function() {
+ 		$(".video-overlay").removeClass('play-me');
 	});
 });
